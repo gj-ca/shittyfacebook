49b46d6c1724cb48173a63aae088254ef921d025
gj-ca <glen.johnson@coderacademy.edu.au>
2021-07-02 14:05:15 +1000

    finishedAuth

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,8 +1,45 @@
+import {useState} from 'react'
+import { useHistory } from 'react-router'
+
 export default function SignUpPage({setAuthenticated}) {
+    // step 1 - declare state
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [flash, setFlash] = useState(false)
+    const {push} = useHistory()
+
+    const dataIsValid = () => {
+        return true
+    }
+    const handleSubmit = event => {
+        event.preventDefault()
+        if (dataIsValid()) {
+            setAuthenticated(true)
+            push("/")
+        } else {
+            setFlash("Incorrect Data")
+        }
+    }
     return (
         <>
             <h1>Sign Up</h1>
-            <button onClick={setAuthenticated}>Sign Up</button>
+            {flash && <p>{flash}</p>}
+            <form onSubmit={handleSubmit}>
+                <div>
+                    {/* Step 2 give input a value and onChange */}
+                    <label>Username:</label>
+                    <input 
+                        value={username} 
+                        onChange={e => setUsername(e.target.value)} />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input 
+                        value={password} 
+                        onChange={e => setPassword(e.target.value)} />
+                </div>
+                <button>Sign Up</button>
+            </form>
         </>
     )
 }
